a94b447dbddabc6472184bde9f0c2c4ecb1b8039
Packy White <pwhite@lyft.com>
2019-04-11 12:30:01 -0700

    take lines apart for multiple metrics per packet

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use packet::ether::{self, Packet as EtherPacket};
 use packet::ip;
 use packet::udp::Packet as UdpPacket;
 use packet::Packet as _;
-use pcap::stream::{PacketCodec};
+use pcap::stream::PacketCodec;
 use pcap::Packet;
-use std::collections::HashMap;
+use std::collections::{hash_map as hash, HashMap};
 use std::convert::From;
 use std::io;
 use std::net::{SocketAddr, SocketAddrV4};
@@ -15,82 +15,102 @@ use tokio::reactor::Handle;
 struct PayloadStream<T: AsRef<[u8]>>(std::marker::PhantomData<T>);
 
 impl<T: AsRef<[u8]>> PayloadStream<T> {
-    fn new() -> PayloadStream<T> {
-        PayloadStream(std::marker::PhantomData)
-    }
+  fn new() -> PayloadStream<T> {
+    PayloadStream(std::marker::PhantomData)
+  }
 }
 
 impl PacketCodec for PayloadStream<Vec<u8>> {
-    type Type = EtherPacket<Vec<u8>>;
+  type Type = EtherPacket<Vec<u8>>;
 
-    fn decode(&mut self, packet: Packet) -> Result<Self::Type, pcap::Error> {
-        let owned = Box::new(packet.data.to_vec());
-        let e = EtherPacket::new(*owned).unwrap();
-        Ok(e)
-    }
+  fn decode(&mut self, packet: Packet) -> Result<Self::Type, pcap::Error> {
+    let owned = Box::new(packet.data.to_vec());
+    let e = EtherPacket::new(*owned).unwrap();
+    Ok(e)
+  }
 }
 
 #[derive(Debug)]
 struct TaggedMetric {
-    source: SocketAddr,
-    metric: statsd_parser::Message,
-    seen: std::time::SystemTime,
+  source: SocketAddr,
+  metric: statsd_parser::Message,
+  seen: std::time::SystemTime,
 }
 
 impl TaggedMetric {
-    fn from_frame<T: AsRef<[u8]>>(p: EtherPacket<T>) -> Result<Self, packet::Error> {
-        let (host, ip_data) = match p.protocol() {
-            ether::Protocol::Ipv4 => {
-                let ip_packet = ip::v4::Packet::new(p.payload())?;
-                Ok((ip_packet.source(), ip_packet.to_owned()))
-            }
-            x => Err(packet::Error::from(format!("Invalid protocol {:?}", x))),
-        }?;
-        let udp_packet = UdpPacket::new(ip_data.split().1)
-            .map_err(|e| packet::Error::from(format!("UDP packet decode error! {:?}", e)))?;
-        let source = SocketAddrV4::new(host, udp_packet.source());
-        let (_header, data) = udp_packet.split();
-        let str_data = String::from_utf8(data.to_vec())
-            .map_err(|e| packet::Error::from(format!("UTF-8 decode error! {:?}", e)))?;
-        let metric = statsd_parser::parse(str_data.trim())
-            .map_err(|e| packet::Error::from(format!("Message decode error! {:?}", e)))?;
-        Ok(TaggedMetric {
+  fn from_frame<T: AsRef<[u8]>>(p: EtherPacket<T>) -> Result<Vec<Self>, packet::Error> {
+    let (host, ip_data) = match p.protocol() {
+      ether::Protocol::Ipv4 => {
+        let ip_packet = ip::v4::Packet::new(p.payload())?;
+        Ok((ip_packet.source(), ip_packet.to_owned()))
+      }
+      x => Err(packet::Error::from(format!("Invalid protocol {:?}", x))),
+    }?;
+    let udp_packet = UdpPacket::new(ip_data.split().1)
+      .map_err(|e| packet::Error::from(format!("UDP packet decode error! {:?}", e)))?;
+    let source = SocketAddrV4::new(host, udp_packet.source());
+    let (_header, data) = udp_packet.split();
+    let str_data = String::from_utf8(data.to_vec())
+      .map_err(|e| packet::Error::from(format!("UTF-8 decode error! {:?}", e)))?;
+    let metrics = str_data
+      .lines()
+      .map(|line| {
+        let metric = statsd_parser::parse(line.trim());
+        match metric {
+          Err(e) => {
+            error!("Line decode error! {:?}", e);
+            None
+          }
+          Ok(metric) => Some(TaggedMetric {
             source: SocketAddr::from(source),
             metric,
             seen: std::time::SystemTime::now(),
-        })
-    }
+          }),
+        }
+      })
+      .flat_map(|x| x);
+    Ok(metrics.collect())
+  }
 }
 
 fn main() -> io::Result<()> {
-    simple_logger::init_with_level(log::Level::Debug).unwrap();
-    info!("starting");
-    let capture = pcap::Capture::from_device("lo").unwrap();
-    let mut capture = capture
-        .open()
-        .expect("Cap open failed")
-        .setnonblock()
-        .expect("cant set nonblocking");
-    capture
-        .filter("port 1234 and udp")
-        .expect("Filter incorrect");
+  simple_logger::init_with_level(log::Level::Debug).unwrap();
+  info!("starting");
+  let capture = pcap::Capture::from_device("lo").unwrap();
+  let mut capture = capture
+    .open()
+    .expect("Cap open failed")
+    .setnonblock()
+    .expect("cant set nonblocking");
+  capture
+    .filter("port 8125 and udp")
+    .expect("Filter incorrect");
 
-    let mut seen_metrics: HashMap<SocketAddr, String> = HashMap::new();
-    let stream = capture
-        .stream(&Handle::default(), PayloadStream::new())
-        .unwrap()
-        .map_err(|_| ())
-        .map(|p| TaggedMetric::from_frame(p));
+  let mut seen_metrics: HashMap<SocketAddr, TaggedMetric> = HashMap::new();
+  let stream = capture
+    .stream(&Handle::default(), PayloadStream::new())
+    .unwrap()
+    .map_err(|_| ())
+    .map(|p| TaggedMetric::from_frame(p));
 
-    let fut = stream.map_err(|_| ()).for_each(move |u| {
-        if u.is_err() {
-            warn!("Bad packet");
-            return Ok(())
+  let fut = stream.map_err(|_| ()).for_each(move |u| {
+    if u.is_err() {
+      warn!("Bad packet");
+      return Ok(());
+    }
+    let u = u.unwrap();
+    for metric in u {
+      match seen_metrics.entry(metric.source) {
+        hash::Entry::Occupied(mut e) => {
+          e.insert(metric);
         }
-        let u = u.unwrap();
-        seen_metrics.entry(u.source).or_insert(format!("{:?}", u));
-        Ok(println!("{:#?}", seen_metrics))
-    });
+        hash::Entry::Vacant(e) => {
+          e.insert(metric);
+        }
+      }
+    }
+    Ok(println!("{:#?}", seen_metrics))
+  });
 
-    Ok(tokio::run(fut.into_future()))
+  Ok(tokio::run(fut.into_future()))
 }
